ca77bd6306d8b72d89dd89ac13fe31487d133844
willlee <willlee_work@126.com>
2017-11-21 01:53:19 +0800

    EF示例程序：无法访问数据库！！！

---
+++ EFTests/EFDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFContext
+{
+    public class EFDbContext : DbContext
+    {
+        public EFDbContext() : base("name=PSP")
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ EFTestsTests/EFDbContextTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EFTests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFContext;
+
+namespace EFTests.Tests
+{
+    [TestClass()]
+    public class EFDbContextTests
+    {
+        [TestMethod()]
+        public void EFDbContextTest()
+        {
+            using (EFDbContext db = new EFDbContext())
+            {
+                List<User> rlst = db.Users.ToList();
+                Assert.IsTrue(rlst.Count > 0);
+            }
+        }
+    }
+}
